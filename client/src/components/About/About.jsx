bd7a81570cfc66c9f857c52608f83b9bb3ead3a9
Esteban Manrupe <esteban_manrupe@hotmail.com>
2022-07-11 14:18:01 -0300

    FRONT: 15vo commit > About modified

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -12,14 +12,14 @@ export default function About() {
             <div className="containerD3">
               <div className="section1">
                 <div className="containerTitAbout">
-                  <span>About the Project...</span>
+                  <h4>About the Project...</h4>
                 </div>
                 <div className="sectionp1">
-                  <p>
+                  <span>
                     Hi everyone! This website was created as an{" "}
                     <b>Individual Project</b> for <b>Henry</b>'s Bootcamp.
-                  </p>
-                  <p>
+                  </span>
+                  {/* <p>
                     The thematic was about <b>COUNTRIES</b>, generating a list
                     of them, exploring the details of each one, applying
                     filters, doing different sorting, and searching through a
@@ -29,7 +29,47 @@ export default function About() {
                     The website also had to be able to create activities, which
                     had to be uploaded to the database, and then be used to
                     filter the countries that had those activities.
-                  </p>{" "}
+                  </p> */}
+                  <span>
+                    The thematic was about <b>Countries of the world</b>,
+                    following certain mandatory guidelines:
+                    <ul>
+                      ◽ Get the information from an external API, and create an
+                      own database to fill it with that information.
+                    </ul>
+                    <ul>
+                      ◽ Create tourist activities through a controlled form,
+                      which verifies data before allowing the upload, and whose
+                      information also was stored in the database.
+                    </ul>
+                    <ul>◽ Include a Landing Page.</ul>
+                    <ul>◽ Display countries with flag, name and continent.</ul>
+                    <ul>
+                      ◽ To be able to sort them alphabetically or by number of
+                      inhabitants.
+                    </ul>
+                    <ul>
+                      ◽ To be able to search for countries that "contain" the
+                      text searched, filter them by Continent and by Activities.
+                    </ul>
+                    <ul>
+                      ◽ Show the detail of each country (capital, surface,
+                      population, etc).
+                    </ul>
+                    <ul>
+                      ◽ Paginated with 9 countries on page 1, and then 10
+                      countries per page.
+                    </ul>
+                    <ul>◽ Use Vanilla CSS.</ul>I added to this:
+                    <ul>◽ Combination of the three filters and sorting. </ul>
+                    <ul>◽ Next and previous buttons.</ul>
+                    <ul>
+                      ◽ 9 possible pop-up messages (to confirm, inform or warn)
+                    </ul>
+                    <ul>
+                      ◽ 1 "Loading..." message while loading information.
+                    </ul>
+                  </span>
                   <div className="containerTech">
                     <div className="containerTech2">
                       <p>Technologies Used:</p>
@@ -55,21 +95,18 @@ export default function About() {
               </div>
               <div className="section2">
                 <div className="containerTitAbout">
-                  <span>About me...</span>
+                  <h4>About me...</h4>
                 </div>
                 <div className="sectionp2">
-                  <p>
+                  <span>
                     My name is <b>Esteban Manrupe</b>, an Electronics Engineer
                     in a full process of reinvention and restiling, with an
                     imminent goal: to become a <b>Full Stack Web Developer</b>.
-                  </p>
+                  </span>
                 </div>
               </div>
               <div className="section3">
-                <p>
-                  I like to share with you these links, so feel free to contact
-                  me:
-                </p>
+                <p>Feel free to contact me on:</p>
                 <a href="https://www.linkedin.com/in/estebanmanrupe/">
                   <img src={linkedin} alt="Lkduser" className="logoLinkedin" />
                 </a>
