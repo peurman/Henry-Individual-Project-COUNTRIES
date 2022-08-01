a168d05c061fd78e6c9beec5def5ffa81205dcc0
Esteban Manrupe <esteban_manrupe@hotmail.com>
2022-08-01 11:54:34 -0300

    noDevTools

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  // composeWithDevTools(applyMiddleware(thunk),
+  applyMiddleware(thunk)
 );
 
 export default store;
